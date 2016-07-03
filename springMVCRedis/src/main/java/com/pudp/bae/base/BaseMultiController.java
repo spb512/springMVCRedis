62dd0aa7776d9ad55b2ceee38579b2ebb050902c
spb <994330982@qq.com>
2016-07-03 12:20:23 +0800

    优化页面

--- springMVCRedis/src/main/java/com/pudp/bae/base/BaseMultiController.java
+++ springMVCRedis/src/main/java/com/pudp/bae/base/BaseMultiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class BaseMultiController {
     protected ModelAndView toView(final String url,final Map<String,Object> map)
     {  
-        ModelAndView  view = new ModelAndView(url);
+        ModelAndView  view = new ModelAndView(url,map);
         return view;
     }
 }
